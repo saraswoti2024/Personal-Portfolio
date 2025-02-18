4316c4fec78abae4a175e783f5677b56b09a66de
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-02-18 18:44:39 +0545

    fieldname added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+gender_field=(
+    ('male',"male"), 
+    ('female','female'),
+    ("other",'other')
+)
+class Users(models.Model):
+    fullname=models.CharField(max_length=50)
+    email=models.EmailField()
+    gender=models.CharField(null=True,max_length=200)
+    message=models.TextField()
