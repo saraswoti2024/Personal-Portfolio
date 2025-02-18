2b0d524ebf06b0eb63afa3c1bde4ef7b7db2ea04
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-02-18 18:45:30 +0545

    saving form datas in  database

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Users
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
+    if request.method=='POST':
+        datas = request.POST
+        fullname1 = datas['namekovalue'] #new_VarName= datas['name_Attributevalue']
+        email1 = datas['email']
+        message1 = datas['message']
+        gender1 = datas['gender']
+        
+        user = Users(fullname=fullname1,email=email1,message=message1,gender=gender1) ##database_ma_Vako_name = mathi_vako_name
+        user.save() #value save database ma
+        return HttpResponse("Submit Successful")
     return render(request,'home.html')
